7fe0ba2c8e43cbc5cc177e96a6aaeb589d1c1d87
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-10-25 23:38:03 -0500

    fix(auth): authorize internal api calls

--- src/main/java/com/techcompany/fastporte/trips/infrastructure/acl/DriverAcl.java
+++ src/main/java/com/techcompany/fastporte/trips/infrastructure/acl/DriverAcl.java
@@ -4,8 +4,10 @@ import com.techcompany.fastporte.shared.utils.EnvironmentConstants;
 import com.techcompany.fastporte.shared.dtos.DriverInformationDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -16,6 +18,9 @@ import java.util.Optional;
 
 @Component
 public class DriverAcl {
+
+    @Value("${internal.api.key}")
+    String INTERNAL_API_KEY;
 
     private final RestTemplate restTemplate;
     private final String BASE_URL = EnvironmentConstants.CURRENT_ENV_URL + "/api/drivers/";
@@ -30,7 +35,13 @@ public class DriverAcl {
         String url = BASE_URL + id;
         logger.info("Driver found: " + url);
         try {
-            DriverInformationDto driverDto = restTemplate.getForObject(url, DriverInformationDto.class);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("X-Internal-API-Key", INTERNAL_API_KEY);
+            HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+            DriverInformationDto driverDto = restTemplate.exchange(url, HttpMethod.GET, entity, DriverInformationDto.class).getBody();
+
             if (driverDto != null) {
                 logger.info("Driver found: " + driverDto.firstLastName());
                 return Optional.of(driverDto);
